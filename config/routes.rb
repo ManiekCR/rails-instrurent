76730cf347e64440e436cabe99501290c85ef545
Sadie Balsom <saralogsin@protonmail.com>
2019-08-19 17:32:24 +0200

    added dashboard controller and also updated routes to match new controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+
+
+  get 'dashboard/show', to: 'dashboard#show'
+
+  resources :instruments
+
   devise_for :users
   root to: 'pages#home'
   # only add routes as you need them
