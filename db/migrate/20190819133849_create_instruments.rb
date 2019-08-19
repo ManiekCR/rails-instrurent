8f26a910054c7695c818f4b3e11fdca79636e16b
Sadie Balsom <saralogsin@protonmail.com>
2019-08-19 15:49:28 +0200

    added booking and instrument models

---
+++ db/migrate/20190819133849_create_instruments.rb
@@ -0,0 +1,12 @@
+class CreateInstruments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :instruments do |t|
+      t.string :name
+      t.string :category
+      t.text :description
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
