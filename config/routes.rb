b776e8d440478276c27ebecb6d5e8e7c7469e0f2
Sadie Balsom <saralogsin@protonmail.com>
2019-08-19 15:52:00 +0200

    added comments to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # only add routes as you need them
+  # discuss in person about which routes are necessary when adding only: and except: cases
 end
