1930caf1457053d6e0cb88d5dac5601f5df97d1a
Marian Caron <caron.marian@gmail.com>
2019-08-20 19:58:49 +0200

    started the show page of dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class DashboardController < ApplicationController
   def show
+    @instruments = Instrument.all
+    @bookings = Booking.all
+  end
+
+  private
+
+  def set_instrument
+
   end
 end
