80725ee20281c3a3cfa563e0b4db3704b2f90204
Esin Ozcan <esinozcan@sabanciuniv.edu>
2019-08-20 16:29:16 +0200

    workingonnew

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   get 'dashboard/show', to: 'dashboard#show'
 
   resources :instruments
-
   devise_for :users
   root to: 'pages#home'
   # only add routes as you need them
