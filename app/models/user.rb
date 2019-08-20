8854da66090b417a647fdf7bdb170a3256df2fa1
Sadie Balsom <saralogsin@protonmail.com>
2019-08-20 18:42:39 +0200

    dependent destroy association attribute added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
   has_many :bookings
-  has_many :instruments
+  has_many :instruments, dependent: :destroy
 end
