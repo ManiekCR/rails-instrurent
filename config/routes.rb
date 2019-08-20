1770d9320741576190a0c592c3f367aab75936a4
Sadie Balsom <saralogsin@protonmail.com>
2019-08-20 19:45:30 +0200

    initiated pundit

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
 
-  # get 'users/sign_out', to: 'pages#home'
-
   get 'dashboard/show', to: 'dashboard#show'
 
   resources :instruments
 
-  devise_for :users
+  devise_for :users do
+  end
   root to: 'pages#home'
   # only add routes as you need them
   # discuss in person about which routes are necessary when adding only: and except: cases
