56861dce24f9fd3fdb205dde310d5f81f1cb5e5a
bartu culha <culhabartu@gmail.com>
2019-08-20 11:16:57 +0200

    added show,new,create methods, and parameters

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,13 +1,32 @@
 class InstrumentsController < ApplicationController
+  skip_before_action :authenticate_user!, only[:index, :show]
   def index
   end
 
   def show
+    @booking = Booking.new
   end
 
   def new
+    @instrument = Instrument.new
+    authorize @instrument
   end
 
   def edit
+  end
+
+  def create
+    @instrument = Instrument.new(instrument_params)
+  end
+
+  private
+
+  def set_instrument
+    @instrument = Instrument.find(params[:id])
+    authorize @instrument
+  end
+
+  def instrument_params
+    params[:instrument].permit(:name, :category, :description)
   end
 end
