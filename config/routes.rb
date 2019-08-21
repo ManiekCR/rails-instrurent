3a81616caa2c8e5154f1a288e20a926003353391
Sadie Balsom <saralogsin@protonmail.com>
2019-08-21 15:49:09 +0200

    made some edits to the style of forms for responsiveness

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-
   get 'dashboard/show', to: 'dashboard#show'
-
   resources :instruments
-  devise_for :users do
-  end
   devise_for :users
   root to: 'pages#home'
 end
