d54d9f4ffe105dbe4d5c38c4dcde35d94897a236
Marian Caron <caron.marian@gmail.com>
2019-08-22 21:12:19 +0200

    updated dashboard - listings - controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,5 @@ class DashboardController < ApplicationController
   skip_after_action :verify_authorized
   def show
     @instruments = current_user.instruments
-    @bookings = current_user.bookings
   end
 end
